e23802d68cdaf832f1e9add709a5ba3bc9a65906
KWADWO TABIRI <KWADWO.TABIRI@oldmutual.com.gh>
2025-06-02 17:44:22 +0000

    loaded .env in server

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ const io = socketIo(server, {
     credentials: true
   }
 });
+require("dotenv").config();
 
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
